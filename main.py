e61ba441250d5f1e01862e9e33f6559bd42ad469
jangcries <jangcries@gmail.com>
2023-11-18 16:23:48 +0800

    2023-11-18 16:23:48

--- main.py
+++ main.py
@@ -22,3 +22,4 @@ if __name__ == "__main__":
 #include "../Includes/WTSTradeDef.hpp"
 #include "../Includes/WTSDataDef.hpp"
 #include "../Includes/WTSVariant.hpp"
+#include "../Includes/IBaseDataMgr.h"
