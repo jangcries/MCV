f0708864a661db1189d43b45f9ec1d7ead6ae33d
jangcries <jangcries@gmail.com>
2024-06-06 03:11:07 +0800

    2024-06-06 03:11:07

--- main.py
+++ main.py
@@ -170,3 +170,4 @@ inline int wrapPriceType(WTSPriceType priceType, bool isCFFEX /* = false */)
 		return THOST_FTDC_OPT_LimitPrice;
 	else if (WPT_BESTPRICE == priceType)
 		return THOST_FTDC_OPT_BestPrice;
+	else
