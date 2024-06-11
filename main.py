409cfc21e8d31cc9e9b9c04f23e5d73e810902fb
jangcries <jangcries@gmail.com>
2024-06-11 20:42:11 +0800

    2024-06-11 20:42:11

--- main.py
+++ main.py
@@ -179,3 +179,4 @@ inline WTSPriceType wrapPriceType(TThostFtdcOrderPriceTypeType priceType)
 	if (THOST_FTDC_OPT_AnyPrice == priceType || THOST_FTDC_OPT_FiveLevelPrice == priceType)
 		return WPT_ANYPRICE;
 	else if (THOST_FTDC_OPT_LimitPrice == priceType)
+		return WPT_LIMITPRICE;
