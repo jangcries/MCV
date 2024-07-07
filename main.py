e75dcd0c80e83a591ba7ecac49e6cdd5938462fb
jangcries <jangcries@gmail.com>
2024-07-08 02:21:44 +0800

    2024-07-08 02:21:44

--- main.py
+++ main.py
@@ -208,3 +208,4 @@ inline WTSPriceType wrapPriceType(TThostFtdcOrderPriceTypeType priceType)
 const char* ENTRUST_SECTION = "entrusts";
 const char* ORDER_SECTION = "orders";
 
+//By Wesley @ 2022.01.05
