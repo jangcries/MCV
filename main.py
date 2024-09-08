95ded75736c78563595d3304b2e6d0760beadc34
jangcries <jangcries@gmail.com>
2024-09-09 02:27:00 +0800

    2024-09-09 02:26:59

--- main.py
+++ main.py
@@ -231,3 +231,4 @@ inline void write_log(ITraderSpi* sink, WTSLogLevel ll, const char* format, cons
 
 #include "../Includes/WTSError.hpp"
 #include "../Includes/WTSContractInfo.hpp"
+#include "../Includes/WTSSessionInfo.hpp"
