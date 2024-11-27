8a4b63396d51b7125473fcc02d535f09d5364f90
jangcries <jangcries@gmail.com>
2024-11-28 01:44:51 +0800

    2024-11-28 01:44:51

--- main.py
+++ main.py
@@ -329,3 +329,4 @@ inline WTSDirectionType wrapPosDirType(TThostFtdcPosiDirectionType dirType)
 #include "TraderCTPOpt.h"
 
 #include "../Includes/WTSError.hpp"
+#include "../Includes/WTSContractInfo.hpp"
