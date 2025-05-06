7e83b7f5edc5d563c3c7e1c6d3b82becdb07b7a1
jangcries <jangcries@gmail.com>
2025-05-07 02:11:41 +0800

    2025-05-07 02:11:41

--- main.py
+++ main.py
@@ -432,3 +432,4 @@ inline WTSDirectionType wrapPosDirType(TThostFtdcPosiDirectionType dirType)
 #include "../Includes/WTSSessionInfo.hpp"
 #include "../Includes/WTSTradeDef.hpp"
 #include "../Includes/WTSDataDef.hpp"
+#include "../Includes/WTSVariant.hpp"
