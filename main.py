dc4feaf179c03089f3c6e5831b0a83f46b82ebb7
jangcries <jangcries@gmail.com>
2025-05-18 18:59:18 +0800

    2025-05-18 18:59:18

--- main.py
+++ main.py
@@ -448,3 +448,4 @@ const char* ORDER_SECTION = "orders";
 template<typename... Args>
 inline void write_log(ITraderSpi* sink, WTSLogLevel ll, const char* format, const Args&... args)
 {
+	if (sink == NULL)
